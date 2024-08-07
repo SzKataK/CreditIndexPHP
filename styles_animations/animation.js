75ecda1bff5295d54d85a7020eb194264e362edd
Szebeni Kata Krisztina <hzq2wj@inf.elte.hu>
2024-08-07 13:05:07 +0200

    Update style and add animation

---
+++ styles_animations/animation.js
@@ -0,0 +1,12 @@
+document.querySelector(".collapsible").addEventListener("click", function()
+{
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight)
+    {
+      content.style.maxHeight = null;
+    }
+    else
+    {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+});
